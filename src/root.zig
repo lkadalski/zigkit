f7c3c56a2fa4ae60cc824966bb4a10ca5a81e9c5
Luke <kadalski.lukasz@gmail.com>
2025-11-19 14:45:06 +0100

    Finished Base64 implementation. Needs rework in free time for decoding part

--- src/root.zig
+++ src/root.zig
@@ -2,3 +2,7 @@ pub const cli = @import("cli.zig");
 pub const encoding = struct {
     pub const base64 = @import("encoding/base64.zig");
 };
+test "import tests from submodules" {
+    _ = cli;
+    _ = encoding.base64;
+}
